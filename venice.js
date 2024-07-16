ccc22acdb370f55b1ab710d6299ff6ceb11c0fab
chrismendez45 <chrismendezm4@gmail.com>
2024-07-16 14:41:58 -0400

    Update venice.js

--- venice.js
+++ venice.js
@@ -14,4 +14,5 @@ L.circle([45.426447715753646, 12.341322503052655], {
   fillcoler: 'lime',
   color: 'green',
   radius: 150
-}).addTo(map);
+}).bindPopup(`<iframe width="560" height="315" src="https://www.youtube.com/embed/k_qxONssH3A?si=nd2gUUbJioBQ_jjp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
+  .addTo(map);
