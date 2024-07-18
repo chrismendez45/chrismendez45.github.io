a6dc3045a25228c285fccd7f6dfc15d816cdf249
chrismendez45 <chrismendezm4@gmail.com>
2024-07-18 14:32:45 -0400

    Create earthquakes.js

---
+++ earthquakes.js
@@ -0,0 +1,21 @@
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"'
+
+d3.json(url).then();
+  function(data)
+    {
+      console.log(data);
+
+      let map = L.map("map", {
+        center: [37.09, -95.71],
+        zoom:5
+    });
+
+        var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/%7Bz%7D/%7By%7D/%7Bx%7D', {
+    maxZoom: 20,
+    attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+}).addTo(map);
+
+
+  }
+  );
+      
